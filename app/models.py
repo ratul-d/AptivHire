42cbb5a54e660426373a39c68366c1cf959d6624
Ratul Dash <ratuldash12@gmail.com>
2025-08-29 21:27:54 +0530

    chore: initial SQLAlchemy & PostgreSQL setup; added models with migration

---
+++ app/models.py
@@ -0,0 +1,46 @@
+from sqlalchemy import Column,Integer,String,Text,ForeignKey,Float
+from .db import Base
+
+class Job(Base):
+    __tablename__ = "jobs"
+
+    id = Column(Integer,primary_key=True,index=True)
+    title = Column(String,nullable=False)
+    summary = Column(Text,nullable=True)
+    skills = Column(Text, nullable=True)
+    experience_required = Column(Text, nullable=True)
+    education_required = Column(Text, nullable=True)
+    responsibilities = Column(Text, nullable=True)
+
+
+class Candidate(Base):
+    __tablename__ = "candidates"
+
+    id = Column(Integer,primary_key=True, nullable=False)
+    name = Column(Text, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    phone = Column(String, nullable=True)
+    skills = Column(Text, nullable=True)
+    education = Column(Text, nullable=True)
+    experience = Column(Text, nullable=True)
+    certifications = Column(Text, nullable=True)
+
+class Match(Base):
+    __tablename__ = "matches"
+
+    id = Column(Integer, primary_key=True, index=True)
+    job_id = Column(Integer, ForeignKey("jobs.id"), nullable=False)
+    candidate_id = Column(Integer, ForeignKey("candidates.id"), nullable=False)
+    match_score = Column(Float, nullable=False)
+    missing_skills = Column(Text, nullable=True)
+
+
+class Interview(Base):
+    __tablename__ = "interviews"
+
+    id = Column(Integer, primary_key=True, index=True)
+    candidate_id = Column(Integer, ForeignKey("candidates.id"), nullable=False)
+    job_id = Column(Integer, ForeignKey("jobs.id"), nullable=False)
+    interview_time = Column(String, nullable=False)
+    format = Column(String, nullable=False)   #EG: "online", "onsite"
+    invite_email = Column(String, nullable=False)
