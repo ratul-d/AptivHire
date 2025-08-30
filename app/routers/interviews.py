4bf9c52a89ee7cc14265c4c10f3df831b03c6637
Ratul Dash <ratuldash12@gmail.com>
2025-08-30 23:29:28 +0530

    refactor: rename endpoint to avoid conflict

--- app/routers/interviews.py
+++ app/routers/interviews.py
@@ -10,7 +10,7 @@ def create_interview(interview: schemas.InterviewBase, db: Session=Depends(get_d
     return crud.create_interview(db=db,interview=interview)
 
 @router.get("/",response_model=list[schemas.Interview])
-def read_interviews_by_id(skip: int=0,limit: int=100,db: Session=Depends(get_db)):
+def read_interviews(skip: int=0,limit: int=100,db: Session=Depends(get_db)):
     return crud.get_interviews(db=db,skip=skip,limit=limit)
 
 @router.get("/{candidate_id}",response_model=schemas.Interview)
